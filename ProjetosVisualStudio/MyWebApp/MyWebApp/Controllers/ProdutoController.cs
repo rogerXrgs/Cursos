0681f6630c6979796c12e85f5f2e59da410d65e9
Roger Silva <rogersharp2012@gmail.com>
2019-03-06 14:09:10 -0300

    Atualizando repositorio com a nova classe de controller do produto.

---
+++ ProjetosVisualStudio/MyWebApp/MyWebApp/Controllers/ProdutoController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyWebApp.AcessoDados;
+using System;
+
+namespace MyWebApp.Controllers
+{
+    [Route("api/[Controller]")]
+    public class ProdutoController : Controller
+    {
+        private readonly IProdutoRepository _produtoRepository;
+
+        public ProdutoController(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_produtoRepository.ListarProdutos());
+            }
+            catch(Exception ex)
+            {
+                return BadRequest("Erro:" + ex.Message);
+            }
+        }
+    }
+}
